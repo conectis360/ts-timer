b0bcec01ee2785e8ede9085b597bfa95c59aff47
James Garcia <conectis360@gmail.com>
2025-02-23 18:49:33 -0300

    feat: creating a new function to use to dispatch the new state using reducer hook

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -38,18 +38,37 @@ type TimersContextProviderProps = {
   children: ReactNode;
 };
 
+//é comum trabalhar com type Action
+
+type Action = {
+  type: "ADD_TIMER" | "START_TIMERS" | "STOP_TIMERS";
+};
+
+function timersReducer(state: TimerState, action: Action): TimerState {
+  //react vai executar essa ação, ele vai te entregar o estado e a ação, é teu trabalho realizar algo com eles
+  
+}
+
 export default function TimersContextProvider({
   children,
 }: TimersContextProviderProps) {
   // responsável por manejar e permitir que usem o contexto
-  useReducer(reducer, initialState);
+
+  //reducer é uma função que é executada automáticamente pelo react quando uma nova ação é executada
+  const [timerState, dispatch] = useReducer(timersReducer, initialState);
 
   const context: TimersContextValue = {
     timers: [],
     isRunning: false,
-    addTimer(timerData) {},
-    startTimer() {},
-    stopTimers() {},
+    addTimer(timerData) {
+      dispatch({ type: "ADD_TIMER" });
+    },
+    startTimer() {
+      dispatch({ type: "START_TIMERS" });
+    },
+    stopTimers() {
+      dispatch({ type: "STOP_TIMERS" });
+    },
   };
   return (
     <TimersContextProvider.Provider value={context}>
