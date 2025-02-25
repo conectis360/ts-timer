b940e3abc3cb50e5dbc8f937d0e3c49e8332ea67
James Garcia <conectis360@gmail.com>
2025-02-24 21:23:26 -0300

    feat: standart react feature like, useEffect, useRef and it's usages

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,18 +1,37 @@
+import { useEffect, useRef } from "react";
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
-  const [remainingState, setRemainingTime] = useState(duration * 1000);
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
 
-  setInterval(function () {
-    setRemainingTime((prevTime) => prevTime - 50);
-  }, 50);
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = (interval.current = setInterval(function () {
+      setRemainingTime((prevTime: number) => prevTime - 50);
+    }, 50));
+
+    interval.current = timer;
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   return (
     <Container as="article">
       <h2>{name}</h2>
       <p>
-        <progress max={duration * 1000} value={remainingState}></progress>
+        <progress
+          max={duration * 1000}
+          value={formattedRemainingTime}
+        ></progress>
       </p>
     </Container>
   );
